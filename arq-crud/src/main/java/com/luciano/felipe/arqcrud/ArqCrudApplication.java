cb119bdd909348522e7e62aab8c0bcc663a7e371
lucia <lucia@DESKTOP-0NIQIBB.mshome.net>
2020-11-23 13:51:11 -0300

    Project arq-crud created

---
+++ arq-crud/src/main/java/com/luciano/felipe/arqcrud/ArqCrudApplication.java
@@ -0,0 +1,13 @@
+package com.luciano.felipe.arqcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArqCrudApplication.class, args);
+	}
+
+}
---
+++ arq-crud/src/test/java/com/luciano/felipe/arqcrud/ArqCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciano.felipe.arqcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArqCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
