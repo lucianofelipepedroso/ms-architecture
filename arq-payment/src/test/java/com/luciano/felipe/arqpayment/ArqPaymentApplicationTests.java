2058da93a5dd6833f6754735ab0a8bde2511367b
Luciano Felipe <luciano.pedroso@live.com>
2020-11-25 21:07:54 -0300

    Project arq-payment create

---
+++ arq-payment/src/test/java/com/luciano/felipe/arqpayment/ArqPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciano.felipe.arqpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArqPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
