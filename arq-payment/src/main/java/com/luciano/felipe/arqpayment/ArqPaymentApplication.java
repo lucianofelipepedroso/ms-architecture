1df79872b59b5eb4f97c4033bde9e591bea1efd4
Luciano Felipe <luciano.pedroso@live.com>
2020-11-25 21:07:00 -0300

    Project arq-payment create

---
+++ arq-payment/src/main/java/com/luciano/felipe/arqpayment/ArqPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.luciano.felipe.arqpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArqPaymentApplication.class, args);
+	}
+
+}
