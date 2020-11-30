cca08b211ccc54313806e89858efa4dca8ed2ab6
Luciano Felipe <luciano.pedroso@live.com>
2020-11-30 14:21:36 -0300

    Project arq-oauth create

---
+++ arq-oauth/src/main/java/com/luciano/felipe/arqoauth/ArqOauthApplication.java
@@ -0,0 +1,13 @@
+package com.luciano.felipe.arqoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArqOauthApplication.class, args);
+	}
+
+}
