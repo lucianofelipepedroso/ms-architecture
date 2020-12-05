a98320deb0869318025db1051ab54b311ff4f042
Luciano Felipe <luciano.pedroso@live.com>
2020-12-05 15:20:06 -0300

    Project arq-discovery create

---
+++ arq-discovery/src/main/java/com/luciano/felipe/discovery/ArqDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.luciano.felipe.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArqDiscoveryApplication.class, args);
+	}
+
+}
