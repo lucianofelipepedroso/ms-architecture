838c47f8781b907b001106d5419c6a1e2dda8b47
Luciano Felipe <luciano.pedroso@live.com>
2020-12-05 20:58:45 -0300

    Finalized

--- arq-gateway/src/main/java/com/luciano/felipe/gateway/ArqGatewayApplication.java
+++ arq-gateway/src/main/java/com/luciano/felipe/gateway/ArqGatewayApplication.java
@@ -1,16 +1,19 @@
-package com.luciano.felipe.arqpayment;
+package com.luciano.felipe.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ArqPaymentApplication {
+@EnableZuulProxy
+public class ArqGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ArqPaymentApplication.class, args);
+		SpringApplication.run(ArqGatewayApplication.class, args);
 	}
 
 }
--- arq-oauth/src/main/java/com/luciano/felipe/arqoauth/ArqOauthApplication.java
+++ arq-oauth/src/main/java/com/luciano/felipe/arqoauth/ArqOauthApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -15,7 +16,7 @@ import com.luciano.felipe.arqoauth.repository.PermissionRepository;
 import com.luciano.felipe.arqoauth.repository.UserRepository;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class ArqOauthApplication {
 
 	public static void main(String[] args) {
